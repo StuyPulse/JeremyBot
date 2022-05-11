2a733938bc9d0c6de49ddfc9d9c936620d2451b9
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-11 12:12:43 -0400

    Add ID to module constants

--- src/main/java/com/stuypulse/robot/constants/Modules.java
+++ src/main/java/com/stuypulse/robot/constants/Modules.java
@@ -10,27 +10,31 @@ public interface Modules {
     double HEIGHT = Units.feetToMeters(30.0);
 
     public interface TopRight {
+        String ID = "Top Right";
         Translation2d LOCATION = new Translation2d(WIDTH * +0.5, HEIGHT * +0.5);
 
-        Module MODULE = new Module(LOCATION);
+        Module MODULE = new Module(ID, LOCATION);
     }
 
     public interface TopLeft {
+        String ID = "Top Left";
         Translation2d LOCATION = new Translation2d(WIDTH * -0.5, HEIGHT * +0.5);
 
-        Module MODULE = new Module(LOCATION);
+        Module MODULE = new Module(ID, LOCATION);
     }
 
     public interface BottomLeft {
+        String ID = "Bottom Left";
         Translation2d LOCATION = new Translation2d(WIDTH * -0.5, HEIGHT * -0.5);
 
-        Module MODULE = new Module(LOCATION);
+        Module MODULE = new Module(ID, LOCATION);
     }
 
     public interface BottomRight {
+        String ID = "Bottom Right";
         Translation2d LOCATION = new Translation2d(WIDTH * +0.5, HEIGHT * -0.5);
 
-        Module MODULE = new Module(LOCATION);
+        Module MODULE = new Module(ID, LOCATION);
     }
 
     Module[] MODULES = {
