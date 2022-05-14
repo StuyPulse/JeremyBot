b75f66d7a5aafcf265c84f2ee5ddb88cb59502dc
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-13 23:01:46 -0400

    remove unused imports

--- src/main/java/com/stuypulse/robot/subsystems/Swerve.java
+++ src/main/java/com/stuypulse/robot/subsystems/Swerve.java
@@ -10,14 +10,12 @@ import com.stuypulse.stuylib.math.Vector2D;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Swerve extends SubsystemBase {
