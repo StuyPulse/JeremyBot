52f76d756a7fba78a1f66a9991c81c645af98069
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-05-14 18:12:32 -0400

    Add DriveControlSim and try to fix TurnControl distance per pulse

--- src/main/java/com/stuypulse/robot/constants/SimModule.java
+++ src/main/java/com/stuypulse/robot/constants/SimModule.java
@@ -3,25 +3,67 @@ package com.stuypulse.robot.constants;
 import com.stuypulse.stuylib.control.Controller;
 import com.stuypulse.stuylib.control.PIDController;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.util.Units;
 
 /**
  * Contains constants for a simulated swerve module
  */
 public interface SimModule {
+	double WHEEL_DIAMETER = Units.inchesToMeters(4.0);
+	double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+
+	public interface Drive {
+		public interface Encoder {
+			double GEARING = 1.0;
+
+			double DISTANCE_PER_PULSE = 1.0;
+
+			int PORT_A = 10;
+			int PORT_B = 11;
+		}
+
+		public interface Feedforward {
+			double kS = 0;
+			double kA = 0;
+			double kV = 0;
+
+			public static SimpleMotorFeedforward getFeedforward() {
+				return new SimpleMotorFeedforward(kS, kV, kA);
+			}
+
+			public static LinearSystem<N1, N1, N1> getPlant() {
+				return LinearSystemId.identifyVelocitySystem(kV, kA);
+			}
+		}
+		
+		public interface Feedback {
+            double kP = 0.01;
+            double kI = 0.0;
+            double kD = 0.005;
+
+            public static Controller getController() {
+                return new PIDController(kP, kI, kD);
+            }
+		}
+	}
+
 	public interface Turn {
 		public interface Encoder {
-			// TODO: input real values
-			double GEAR_RATIO = 1;
-			double DISTANCE_PER_PULSE = 1;
+			double GEAR_RATIO = 1.0;
+
+			double GRAYHILL_PULSES_PER_REVOLUTION = 256;
+			double GRAYHILL_DISTANCE_PER_PULSE =
+					(WHEEL_CIRCUMFERENCE / GRAYHILL_PULSES_PER_REVOLUTION);
 
 			int PORT_A = 0;
 			int PORT_B = 1;
 		}
 
-		public interface SysID {
+		public interface Feedforward {
 			double kA = 0.5;
 			double kV = 0.1;
 
--- src/main/java/com/stuypulse/robot/subsystems/swerve/sim/TurnControlSim.java
+++ src/main/java/com/stuypulse/robot/subsystems/swerve/sim/TurnControlSim.java
@@ -1,5 +1,6 @@
 package com.stuypulse.robot.subsystems.swerve.sim;
 
+import com.stuypulse.robot.constants.SimModule;
 import com.stuypulse.robot.constants.SimModule.Turn;
 import com.stuypulse.robot.subsystems.swerve.TurnControl;
 
@@ -18,29 +19,35 @@ public class TurnControlSim extends TurnControl {
 		super(Turn.Feedback.getController());
 
 		turn = new FlywheelSim(
-			Turn.SysID.getPlant(),
+			Turn.Feedforward.getPlant(),
 			DCMotor.getNEO(1),
 			Turn.Encoder.GEAR_RATIO
 		);
 
 		encoder = new Encoder(Turn.Encoder.PORT_A, Turn.Encoder.PORT_B);
-		encoder.setDistancePerPulse(Turn.Encoder.DISTANCE_PER_PULSE);
+		encoder.setDistancePerPulse(Turn.Encoder.GRAYHILL_DISTANCE_PER_PULSE);
 
 		encoderSim = new EncoderSim(encoder);
 	}
 
+	private double getRadians() {
+		double rotations = encoder.getDistance() / SimModule.WHEEL_CIRCUMFERENCE;
+
+		return rotations / (2 * Math.PI);
+	}
+
 	@Override
 	public Rotation2d getAngle() {
-		
+		return new Rotation2d(getRadians());
 	}
 
 	@Override
 	protected void setVoltage(double voltage) {
-		
+		turn.setInputVoltage(voltage);	
 	}
 
 	@Override
 	protected void reset() {
-		
+		encoderSim.setCount(0);
 	}
 }
