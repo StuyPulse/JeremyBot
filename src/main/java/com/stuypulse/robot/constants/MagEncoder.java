0859c47f05ffcc585dd52ed0d57a65f27bc96110
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-23 22:59:53 -0400

    Add to the absolute encoder module [not working]

---
+++ src/main/java/com/stuypulse/robot/constants/MagEncoder.java
@@ -0,0 +1,6 @@
+package com.stuypulse.robot.constants;
+
+public interface MagEncoder {
+    
+    double CONVERSION = 2.0 * Math.PI;
+}
--- src/main/java/com/stuypulse/robot/subsystems/swerve/neo/NEOMagTurnControl.java
+++ src/main/java/com/stuypulse/robot/subsystems/swerve/neo/NEOMagTurnControl.java
@@ -1,26 +1,54 @@
 package com.stuypulse.robot.subsystems.swerve.neo;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.stuypulse.robot.constants.MagEncoder;
+import com.stuypulse.robot.constants.NEOModule.Turn;
+import com.stuypulse.robot.subsystems.swerve.TurnControl;
+import com.stuypulse.robot.util.NEOConfig;
+
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
-public class NEOMagTurnControl extends NEOTurnControl {
-    private final DutyCycleEncoder absoluteEncoder;
+public class NEOMagTurnControl extends TurnControl {
+    private final CANSparkMax turn;
+    private final DutyCycleEncoder encoder;
 
-    public NEOMagTurnControl(int turnPort, int encoderPort) {
-        super(turnPort);
-        absoluteEncoder = new DutyCycleEncoder(encoderPort);
+    public NEOMagTurnControl(int port, int encoderPort) {
+        super(Turn.Feedback.getController());
+
+        turn = new CANSparkMax(port, MotorType.kBrushless);
+        
+        encoder = new DutyCycleEncoder(encoderPort);
+
+        
+        configure(Turn.getConfig());
     }
 
-    // public NEOMagTurnControl configure(DutyCycleEncoderConfig config) {
-    // }
+    public NEOMagTurnControl configure(NEOConfig config) {
+        config.setup(turn);
+        return this;
+    }
+
+    private double getRadians() {
+        return encoder.getAbsolutePosition() * MagEncoder.CONVERSION;
+    }
 
     @Override
     public Rotation2d getAngle() {
-        return null;
+        return new Rotation2d(getRadians());
+    }
+
+    @Override
+    protected void setVoltage(double voltage) {
+        turn.setVoltage(voltage);
     }
 
     @Override
     protected void reset() {
+        DriverStation.reportWarning("Trying to reset absolute encoder, don't know what this means?", false);
+        // encoder.reset();
+        // encoder.setPosition(0);
     }
-    
 }
