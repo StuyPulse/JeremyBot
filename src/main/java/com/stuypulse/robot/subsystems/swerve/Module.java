a0ede6ad7f34dbc21d30c827a6a760747e5ec96f
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-15 19:06:08 -0400

    Remove unused imports

--- src/main/java/com/stuypulse/robot/subsystems/swerve/Module.java
+++ src/main/java/com/stuypulse/robot/subsystems/swerve/Module.java
@@ -3,8 +3,6 @@ package com.stuypulse.robot.subsystems.swerve;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Module extends SubsystemBase {
