827f56b79ed85bb1b6468cca9e4769ff5431fe92
anivanchen <ivanchen07@gmail.com>
2022-07-06 01:10:41 -0400

    Create 2 metre, 4, 5, 6 ball autons
    Co-authored-by: vincentw <vinowang921@gmail.com>

---
+++ src/main/java/com/stuypulse/robot/commands/autos/TwoMetresAuto.java
@@ -0,0 +1,24 @@
+package com.stuypulse.robot.commands.autos;
+
+import com.stuypulse.robot.RobotContainer;
+import com.stuypulse.robot.commands.FollowTrajectory;
+import com.stuypulse.robot.util.TrajectoryLoader;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+/**
+ * @author Ivan Chen
+ * @author Vincent Wang
+ */
+
+public class TwoMetresAuto extends SequentialCommandGroup {
+
+    private static final Trajectory TO_INFINITY_AND_BEYOND = TrajectoryLoader.getTrajectory("TwoMetresAuto/output/DriveForward.wpilib.json");
+    
+    public TwoMetresAuto(RobotContainer robot) {
+        addCommands(
+            new FollowTrajectory(robot.swerve, TO_INFINITY_AND_BEYOND).robotRelative()
+        );
+    }
+}
