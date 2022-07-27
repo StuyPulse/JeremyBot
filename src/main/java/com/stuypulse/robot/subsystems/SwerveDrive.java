f2366afa119fb2e1dc5b6ed565c36590d27a182f
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-26 22:20:19 -0400

    remove space from swerve drive

--- src/main/java/com/stuypulse/robot/subsystems/SwerveDrive.java
+++ src/main/java/com/stuypulse/robot/subsystems/SwerveDrive.java
@@ -2,7 +2,7 @@ package com.stuypulse.robot.subsystems;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
-                    
+
 import com.kauailabs.navx.frc.AHRS;
 import com.stuypulse.robot.constants.Settings;
 import com.stuypulse.stuylib.math.Vector2D;
@@ -20,17 +20,17 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-                    
+
 public class SwerveDrive extends SubsystemBase {
-                    
+
     /** CONSTANTS **/
-                    
+
     private interface Chassis {
         double WIDTH = Units.inchesToMeters(29.0);
         double HEIGHT = Units.inchesToMeters(29.0);
         double MAX_SPEED = Units.feetToMeters(14.0);
     }
-    
+
     private interface FrontRight {
         String ID = "Front Right";
         int DRIVE_PORT = 1;
@@ -39,7 +39,7 @@ public class SwerveDrive extends SubsystemBase {
         Rotation2d ABSOLUTE_OFFSET = Rotation2d.fromDegrees(127.65);
         Translation2d MODULE_OFFSET = new Translation2d(Chassis.WIDTH * +0.5, Chassis.HEIGHT * -0.5);
     }
-                    
+
     private interface FrontLeft {
         String ID = "Front Left";
         int DRIVE_PORT = 3;
@@ -48,7 +48,7 @@ public class SwerveDrive extends SubsystemBase {
         Rotation2d ABSOLUTE_OFFSET = Rotation2d.fromDegrees(-148.8);
         Translation2d MODULE_OFFSET = new Translation2d(Chassis.WIDTH * +0.5, Chassis.HEIGHT * +0.5);
     }
-    
+
     private interface BackLeft {
         String ID = "Back Left";
         int DRIVE_PORT = 5;
@@ -57,7 +57,7 @@ public class SwerveDrive extends SubsystemBase {
         Rotation2d ABSOLUTE_OFFSET = Rotation2d.fromDegrees(-34.2);
         Translation2d MODULE_OFFSET = new Translation2d(Chassis.WIDTH * -0.5, Chassis.HEIGHT * +0.5);
     }
-    
+
     private interface BackRight {
         String ID = "Back Right";
         int DRIVE_PORT = 7;
@@ -78,7 +78,7 @@ public class SwerveDrive extends SubsystemBase {
     private final SwerveDriveOdometry odometry;
                     
     private final Field2d field;
-                    
+
     public SwerveDrive() {
         
         modules = new WPI_SimModule[] {
@@ -106,36 +106,36 @@ public class SwerveDrive extends SubsystemBase {
                     
         reset(new Pose2d());
     }
-                    
+
     /** MODULE API **/
-                    
+
     public WPI_SimModule getModule(String id) {
         for (WPI_SimModule module : modules) {
             if (module.getId().equals(id)) 
                 return module;
         }
-        
+
         throw new IllegalArgumentException("Couldn't find module with ID \"" + id + "\"");
     }
-                    
+
     public WPI_SimModule[] getModules() {
         return modules;
     }
-                    
+
     public Stream<WPI_SimModule> getModuleStream() {
         return Arrays.stream(getModules());
     }
-                    
+
     public void reset(Pose2d pose) {
         odometry.resetPosition(pose, getGyroAngle()); 
     }
-                    
+
     public void reset() {
         reset(getPose());
     }
-                    
+
     /** MODULE STATES API **/
-                    
+
     public void setStates(Vector2D velocity, double omega, boolean fieldRelative) {
         if (fieldRelative) {
             // try correcting turn angle in simulation
@@ -145,22 +145,22 @@ public class SwerveDrive extends SubsystemBase {
             setStates(new ChassisSpeeds(velocity.y, -velocity.x, -omega));
         }
     }
-                    
+
     public void setStates(Vector2D velocity, double omega) {
         setStates(velocity, omega, true);
     }
-                    
+
     public void setStates(ChassisSpeeds robotSpeed) {
         setStates(kinematics.toSwerveModuleStates(robotSpeed));
     }
-                    
+
     public void setStates(SwerveModuleState... states) {
         if (states.length != modules.length) {
             throw new IllegalArgumentException("Number of desired module states does not match number of modules (" + modules.length + ")");
         }
-                    
+
         SwerveDriveKinematics.desaturateWheelSpeeds(states, Chassis.MAX_SPEED);
-        
+
         for (int i = 0; i < states.length; ++i) {
             modules[i].setTargetState(states[i]);
         }
@@ -176,30 +176,30 @@ public class SwerveDrive extends SubsystemBase {
     private void updateOdometry() {
         odometry.update(
             getGyroAngle(), 
-    
+
             getModuleStream()
                 .map(x -> x.getState())
                 .toArray(SwerveModuleState[]::new)
         );
     }
-       
+
     public Pose2d getPose() {
         return odometry.getPoseMeters();
     }
-                    
+
     public Rotation2d getAngle() {
         return getPose().getRotation();
     }
-                    
+
     public SwerveDriveKinematics getKinematics() {
         return kinematics;
     }
-                    
+
     @Override
     public void periodic() {
         updateOdometry();
         field.setRobotPose(getPose());
-        
+
         SmartDashboard.putNumber("Swerve/Pose X", getPose().getTranslation().getX());
         SmartDashboard.putNumber("Swerve/Pose Y", getPose().getTranslation().getY());
         SmartDashboard.putNumber("Swerve/Pose Angle", getAngle().getDegrees());
@@ -211,9 +211,8 @@ public class SwerveDrive extends SubsystemBase {
         // Integrate omega in simulation and store in gyro
         var states = getModuleStream().map(x -> x.getState()).toArray(SwerveModuleState[]::new);
         var speeds = getKinematics().toChassisSpeeds(states);
-        
+
         gyro.setAngleAdjustment(gyro.getAngle() + Math.toDegrees(speeds.omegaRadiansPerSecond * Settings.dT));
     }
-                    
+
 }
-                    
