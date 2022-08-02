15a8272c460ebf233db7fb0a6674d9d80b4949ef
Myles Pasetsky <mp3blue99@gmail.com>
2022-08-02 14:46:35 -0400

    remove unnecessary import

--- src/main/java/com/stuypulse/robot/subsystems/SwerveDrive.java
+++ src/main/java/com/stuypulse/robot/subsystems/SwerveDrive.java
@@ -15,7 +15,6 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
