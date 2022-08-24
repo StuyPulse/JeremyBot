c66116e0e58a0ca387ee159d77e76cb9f658c1ca
BenG49 <64862590+BenG49@users.noreply.github.com>
2022-08-24 19:41:43 -0400

    Update SnapDrive to use AStick

--- src/main/java/com/stuypulse/robot/commands/SnapDrive.java
+++ src/main/java/com/stuypulse/robot/commands/SnapDrive.java
@@ -8,9 +8,9 @@ import com.stuypulse.stuylib.control.feedforward.Feedforward;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.math.Angle;
 import com.stuypulse.stuylib.network.SmartNumber;
+import com.stuypulse.stuylib.streams.angles.AStick;
 import com.stuypulse.stuylib.streams.angles.AStream;
 import com.stuypulse.stuylib.streams.angles.filters.AMotionProfile;
-import com.stuypulse.stuylib.streams.booleans.BStream;
 import com.stuypulse.stuylib.streams.vectors.VStream;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -20,7 +20,7 @@ public class SnapDrive extends CommandBase {
     /** CONSTANTS **/
 
     public interface Turn {
-        SmartNumber DEADBAND = new SmartNumber("Controls/Snap/Deadband", 0.5);
+        SmartNumber DEADZONE = new SmartNumber("Controls/Snap/Deadband", 0.5);
     }
 
     public interface FF {
@@ -36,8 +36,8 @@ public class SnapDrive extends CommandBase {
     }
 
     public interface Profile {
-        SmartNumber accelLimit = new SmartNumber("Controls/Snap/Accel Limit", 2);
-        SmartNumber jerkLimit = new SmartNumber("Controls/Snap/Jerk Limit", 10);
+        SmartNumber ACCEL_LIMIT = new SmartNumber("Controls/Snap/Accel Limit", 2);
+        SmartNumber JERK_LIMIT = new SmartNumber("Controls/Snap/Jerk Limit", 10);
     }
 
 
@@ -45,7 +45,6 @@ public class SnapDrive extends CommandBase {
 
     private final VStream drive;
     private final AStream turn;
-    private final BStream deadZone;
 
     private final AngleController control;
 
@@ -53,13 +52,10 @@ public class SnapDrive extends CommandBase {
         this.swerve = swerve;
 
         drive = VStream.create(gamepad::getLeftStick).filtered(Drive.getFilter());
-        turn = AStream.create(
-            VStream.create(gamepad::getRightStick))
-                .filtered(x -> x.sub(Angle.k90deg));
-        deadZone = BStream.create(() -> {return gamepad.getRightStick().magnitude() < Turn.DEADBAND.get();});
+        turn = new AStick(gamepad::getRightStick, Turn.DEADZONE);
         
         control = new Feedforward.Motor(FF.kA, FF.kS, FF.kV).angle()
-            .setSetpointFilter(new AMotionProfile(Profile.accelLimit, Profile.jerkLimit))
+            .setSetpointFilter(new AMotionProfile(Profile.ACCEL_LIMIT, Profile.JERK_LIMIT))
             .add(new AnglePIDController(Feedback.kP, Feedback.kI, Feedback.kD));
 
         addRequirements(swerve);
@@ -70,10 +66,7 @@ public class SnapDrive extends CommandBase {
     
     @Override
     public void execute() {
-        double omega = 0;
-        if (!deadZone.get()) {
-            omega = control.update(turn.get(), Angle.fromRotation2d(swerve.getGyroAngle()));
-        }
+        double omega = control.update(turn.get(), Angle.fromRotation2d(swerve.getGyroAngle()));
 
         swerve.setStates(drive.get(), omega);
     }
