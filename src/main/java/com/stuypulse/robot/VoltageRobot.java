6cb9d6cec2126a20e505578af5f77d87a658b8cf
Myles Pasetsky <mp3blue99@gmail.com>
2022-11-07 12:27:14 -0500

    replace voltage robot container with voltagerobot

--- src/main/java/com/stuypulse/robot/VoltageRobot.java
+++ src/main/java/com/stuypulse/robot/VoltageRobot.java
@@ -4,40 +4,27 @@
 
 package com.stuypulse.robot;
 
-import com.stuypulse.robot.commands.*;
-import com.stuypulse.robot.constants.Ports;
-import com.stuypulse.robot.commands.autos.*;
-import com.stuypulse.robot.subsystems.SwerveDrive;
-import com.stuypulse.robot.subsystems.VoltageSwerveModule;
-import com.stuypulse.robot.util.BootlegXbox;
-import com.stuypulse.stuylib.input.Gamepad;
-import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
-import com.stuypulse.stuylib.input.gamepads.PS4;
-import com.stuypulse.stuylib.input.gamepads.keyboard.SimKeyGamepad;
+import com.stuypulse.robot.constants.Settings;
+import com.stuypulse.robot.subsystems.modules.VoltageSwerveModule;
 import com.stuypulse.stuylib.math.Angle;
-import com.stuypulse.stuylib.math.Vector2D;
 import com.stuypulse.stuylib.network.SmartAngle;
+import com.stuypulse.stuylib.network.SmartNumber;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.TimedRobot;
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
  */
-public class VoltageRobotContainer {
+public class VoltageRobot extends TimedRobot {
 
     private interface Chassis {
         double WIDTH = Units.inchesToMeters(29.0);
@@ -87,7 +74,7 @@ public class VoltageRobotContainer {
     }
 
     // Subsystems
-    public final VoltageSwerveModule[] modules = new VoltageSwerveModule[] {
+    private final VoltageSwerveModule[] modules = new VoltageSwerveModule[] {
             makeModule(FrontRight.ID, FrontRight.TURN_PORT, FrontRight.DRIVE_PORT,
                     FrontRight.ENCODER_PORT, FrontRight.ABSOLUTE_OFFSET, FrontRight.MODULE_OFFSET),
             makeModule(FrontLeft.ID, FrontLeft.TURN_PORT, FrontLeft.DRIVE_PORT,
@@ -98,45 +85,78 @@ public class VoltageRobotContainer {
                     BackRight.ENCODER_PORT, BackRight.ABSOLUTE_OFFSET, BackRight.MODULE_OFFSET)
     };
 
-    // Gamepads
-    // public final Gamepad driver = new SimKeyGamepad(); // new
-    // BootlegXbox(Ports.Gamepad.DRIVER);
-    public final Gamepad driver = new PS4(0);
-    public final Gamepad test = new AutoGamepad(Ports.Gamepad.TEST);
+    private SmartNumber voltage;
 
-    // Autons
-    private static SendableChooser<Command> autonChooser = new SendableChooser<>();
-
-    public VoltageRobotContainer() {
-        // Disabling joystick connect allows for AutoGamepad to not be noisy
-        DriverStation.silenceJoystickConnectionWarning(true);
-
-        // Configure the button bindings
-        configureDefaultCommands();
-        configureButtonBindings();
-        configureAutons();
-    }
-
-    private void configureDefaultCommands() {
-
-        // swerve.setDefaultCommand(new InstantCommand(() -> swerve.setStates(new
-        // Vector2D(0, 0.5), 0)));
-    }
-
-    private void configureButtonBindings() {
-    }
-
-    public void configureAutons() {
-        SmartDashboard.putData("Autonomous", autonChooser);
+    public VoltageRobot() {
+        super(Settings.dT);
     }
 
     /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
      */
-    public Command getAutonomousCommand() {
-        return autonChooser.getSelected();
+    @Override
+    public void robotInit() {
+        voltage = new SmartNumber("Voltage", 0.0);
+        DataLogManager.start();
     }
 
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+    }
+
+    /** This function is called once each time the robot enters Disabled mode. */
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    /**
+     * This autonomous runs the autonomous command selected by your
+     * {@link RobotContainer} class.
+     */
+    @Override
+    public void autonomousInit() {
+    }
+
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+    }
+
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+        for (var module : modules) {
+            module.setVoltage(voltage.get());
+        }
+    }
+
+    @Override
+    public void testInit() {
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
 }
