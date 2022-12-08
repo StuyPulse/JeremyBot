275cc08492041c0ae3120ef3b2bb4cb22968478e
Myles Pasetsky <mp3blue99@gmail.com>
2022-12-08 15:55:03 -0500

    Add non-breaking drive command

--- src/main/java/com/stuypulse/robot/commands/DriveCommand.java
+++ src/main/java/com/stuypulse/robot/commands/DriveCommand.java
@@ -1,17 +1,13 @@
 package com.stuypulse.robot.commands;
 
-import com.stuypulse.robot.constants.Settings.Robot;
 import com.stuypulse.robot.subsystems.SwerveDrive;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.math.SLMath;
 import com.stuypulse.stuylib.streams.IStream;
-import com.stuypulse.stuylib.streams.filters.Derivative;
-import com.stuypulse.stuylib.streams.filters.LowPassFilter;
 import com.stuypulse.stuylib.streams.vectors.VStream;
 import com.stuypulse.stuylib.streams.vectors.filters.VDeadZone;
-import com.stuypulse.stuylib.streams.vectors.filters.VRateLimit;
+import com.stuypulse.stuylib.streams.vectors.filters.VLowPassFilter;
 
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class DriveCommand extends CommandBase {
@@ -20,28 +16,19 @@ public class DriveCommand extends CommandBase {
     private VStream speed;
     private IStream turn;
 
-    private IStream i;
-
     public DriveCommand(SwerveDrive drive, Gamepad driver) {
         this.drive = drive;
 
-        i = new IStream() {
-            Derivative d = new Derivative();
-            @Override
-            public double get() {
-                return Robot.Control.poop.maxAchievableVelocity(RobotController.getBatteryVoltage(), d.get(drive.getVelocity()));
-            }
-
-        };
-
         speed = VStream.create(driver::getLeftStick).filtered(
-            new VDeadZone(0.1),
-            x -> x.mul(i.get()),
-            new VRateLimit(IStream.create(() -> Robot.Control.poop.maxAchievableAcceleration(RobotController.getBatteryVoltage(), drive.getVelocity())).number())
+            new VDeadZone(0.05),
+            new VLowPassFilter(0.1),
+            x -> x.mul(4.2)
         );
 
-        // speed = VStream.create(driver::getLeftStick).filtered(x -> x.mul(4.2));
-        turn = IStream.create(driver::getRightX).filtered(x -> SLMath.deadband(x, 0.1), new LowPassFilter(0.2), x -> x * 6);
+        turn = IStream.create(driver::getRightX).filtered(
+            x -> SLMath.deadband(x, 0.05), 
+            x -> x * 6
+        );
 
         addRequirements(drive);
     }
